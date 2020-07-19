f9e0c5cbdf6180e2345729c47823404746352de8
andratitano <andrabusra10@gmail.com>
2020-07-20 02:08:13 +0700

    Initial commit

---
+++ app/src/main/java/com/andra/synrgy_mengenalviewgroup/CardViewActivity.kt
@@ -0,0 +1,12 @@
+package com.andra.synrgy_mengenalviewgroup
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class CardViewActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_view)
+    }
+}
---
+++ app/src/main/java/com/andra/synrgy_mengenalviewgroup/FrameLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.andra.synrgy_mengenalviewgroup
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class FrameLayoutActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame_layout)
+    }
+}
